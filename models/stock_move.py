90df1b1466b2c655e732662d0797897d6755ff99
hannan-bhatti <hannaninvalhalla@gmail.com>
2026-07-11 16:34:01 +0500

    Changes in plan

--- models/stock_move.py
+++ models/stock_move.py
@@ -1,7 +1,7 @@
-from odoo import models, fields # type: ignore
+from odoo import models, fields
 
 class StockMove(models.Model):
     _inherit = 'stock.move'
 
-    az_gross_weight = fields.Float(string="G.W")
-    az_net_weight = fields.Float(string="N.W")
+    # The custom gross weight field has been removed.
+    az_net_weight = fields.Float(string="Net weight")
